0f8eb8c899cbd3eecb861b97a9d013683244857a
zzy0371 <496575233@qq.com>
2021-01-22 10:21:36 +0800

    Flask的模板

---
+++ project1/demo3.py
@@ -0,0 +1,16 @@
+"""
+Flask的模板文件
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    name = "qiku"
+    age = 5
+    return render_template("index.html", **locals())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
